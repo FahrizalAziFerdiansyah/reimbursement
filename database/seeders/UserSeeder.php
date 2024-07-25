e5891d43d07cc26e1145abc0b4dc00a1dafcf4f0
Fahrizal AziFerdiansyah <fahrizalaziferdiansyah@Fahrizals-MacBook-Air.local>
2024-07-25 22:13:05 +0700

    [+] seeder : role and user

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,20 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $director = User::firstOrCreate([
+            'nip' => '1234',
+            'name' => 'DONI',
+            'position' => 'DIREKTUR',
+            'password' => bcrypt(123456)
+        ]);
+        $director->assignRole('director');
+
+        $finance = User::firstOrCreate([
+            'nip' => '1235',
+            'name' => 'DONO',
+            'position' => 'FINANCE',
+            'password' => bcrypt(123456)
+        ]);
+        $finance->assignRole('finance');
     }
 }
